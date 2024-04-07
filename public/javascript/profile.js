d61c498f4a1eb8ae0db1634f89b5647de8d21f43
Angela Yu <ayu107@my.bcit.ca>
2024-04-06 22:03:20 -0700

    add comments to js and css files

--- public/javascript/profile.js
+++ public/javascript/profile.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const alertList = document.getElementById('alertList');
     const createAlertButton = document.querySelector('#createAlertModal button.btn-green');
 
+    //event listener for createalert button 
     createAlertButton.addEventListener('click', async function () {
         const fromCurrency = document.getElementById('fromCurrency').value;
         const toCurrency = document.getElementById('toCurrency').value;
@@ -53,7 +54,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Create a new rate alert box
         const alertBox = document.createElement('div');
         alertBox.classList.add('rounded', 'border', 'p-3','mb-3', 'alert-item');
-        alertBox.dataset.alertId = alertId;
+        alertBox.dataset.alertId = alertId; //assign unique alertId to each alert 
         alertBox.innerHTML = `
             <div>1 ${fromCurrency} = ${targetRate} ${toCurrency} </div>
         `;
@@ -61,6 +62,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Append the new rate alert box to the alertList
         alertList.appendChild(alertBox);
 
+        //reload the page
         setTimeout(() => {
             location.reload();
         }, 10);
@@ -68,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Hide the modal
         $('#createAlertModal').modal('hide');
 
-        //send the data to the server
+        //send the data to the server and store in DB
         try {
             await fetch('/profile', {
                 method: 'POST',
@@ -83,6 +85,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
 
+    //reset alert modal values
     $('#createAlertModal').on('hidden.bs.modal', function () {
         document.getElementById('fromCurrency').value = '';
         document.getElementById('toCurrency').value = '';
@@ -92,21 +95,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-//function to delete items from db
+//event listener for delete alert button
 document.addEventListener('DOMContentLoaded', function () {
     const deleteButtons = document.querySelectorAll('.delete-alert');
     const confirmDeleteModal = new bootstrap.Modal(document.getElementById('confirmDeleteModal'));
 
+    //retrieve alertId for each alert and add listener to its delete button 
     deleteButtons.forEach(button => {
         button.addEventListener('click', async function () {
             const alertId = button.parentElement.getAttribute('data-alert-id');
             console.log('Deleting alert with ID:', alertId);
 
+            //show confirmation delete modal
             confirmDeleteModal.show();
 
             const confirmDeleteButton = document.getElementById('confirmDeleteButton');
-            confirmDeleteButton.addEventListener('click', async function () {
 
+            //add event listener to confirm delete button in modal
+            confirmDeleteButton.addEventListener('click', async function () {
                 try {
                     const response = await fetch(`/profile/${alertId}`, {
                         method: 'DELETE'
